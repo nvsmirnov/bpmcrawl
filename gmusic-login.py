6397a9cec02fbbd84564b22f9aeaaa4978fdb562
Nikita Smirnov <nsmirnov@gmail.com>
2020-06-23 14:03:04 +0300

    output format

--- gmusic-login.py
+++ gmusic-login.py
@@ -12,5 +12,6 @@ api = Mobileclient()
 if not api.oauth_login(gmusic_client_id):
     print(f"oauth: {api.perform_oauth()}")
 else:
-    print(f"already logged in (you may remove ~.local/share/gmusicapi/mobileclient.cred to relogin, or modify this script to handle logoffs more correctly)")
+    print()
+    print(f"already logged in (you may remove ~/.local/share/gmusicapi/mobileclient.cred to relogin, or modify this script to handle logoffs more correctly)")
 
