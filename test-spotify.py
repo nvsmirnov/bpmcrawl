29c5aca95ea54166191429a8a5fb8f3476dfdcc9
Nikita Smirnov <nsmirnov@gmail.com>
2020-10-01 17:19:28 +0300

    spotify works

--- test-spotify.py
+++ test-spotify.py
@@ -1,8 +1,13 @@
 import spotipy
-
 from spotipy.oauth2 import SpotifyOAuth
-
 from pprint import pprint
+from music_api import *
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("spotipy").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("sqlitedict").setLevel(logging.ERROR)
 
 # Set these environment variables:
 # SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET
@@ -15,35 +20,32 @@ from pprint import pprint
 # URI will fail to open, bu you'll need to copy URI from browser address bar to script's input. And that's it.
 # P.S. don't know yet where it caches authorization token and how to clear it.
 
-# set open_browser=False to prevent Spotipy from attempting to open the default browser
-spotify_scopes = [
-    "playlist-read-collaborative",
-    "playlist-read-private",
-    "playlist-modify-public",
-    "playlist-modify-private",
-    "user-library-read",
-    "user-library-modify",
-    "user-top-read",
-    "user-follow-read",
-    "user-read-recently-played"
-]
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(open_browser=False, scope=" ".join(spotify_scopes)))
-print(sp.me())
-
+## set open_browser=False to prevent Spotipy from attempting to open the default browser
+#spotify_scopes = [
+#    "playlist-read-collaborative",
+#    "playlist-read-private",
+#    "playlist-modify-public",
+#    "playlist-modify-private",
+#    "user-library-read",
+#    "user-library-modify",
+#    "user-top-read",
+#    "user-follow-read",
+#    "user-read-recently-played"
+#]
+#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(open_browser=False, scope=" ".join(spotify_scopes)))
+#print(sp.me())
+#
 #results = sp.current_user_playlists(limit=50)
+###results = sp.next(results)
 #for i, item in enumerate(results['items']):
-#    print("%d %s" % (i, item['name']))
-#    #from pprint import pprint
-#    #pprint(item)
-
+#    print(f"{i}, {item['id']}, {item['name']}")
+#
 #response = sp.featured_playlists()
 #print(response['message'])
-
 #while response:
 #    playlists = response['playlists']
 #    for i, item in enumerate(playlists['items']):
-#        print(playlists['offset'] + i, item['name'])
-#
+#        print(f"{playlists['offset'] + i}, {item['id']}, {item['name']}")
 #    if playlists['next']:
 #        response = sp.next(playlists)
 #    else:
@@ -52,24 +54,37 @@ print(sp.me())
 #pl = sp.playlist("37i9dQZF1E380nDmsOSXst")
 #pprint(pl)
 
-print("Getting playlist...")
-tracks = []
-result = sp.playlist_items("37i9dQZF1DXdbXrPNafg9d", additional_types=['track'])
-tracks.extend(result['items'])
-# if playlist is larger than 100 songs, continue loading it until end
-while result['next']:
-    print("Continuing...")
-    result = sp.next(result)
-    tracks.extend(result['items'])
-# remove all local songs
-i = 0  # just for counting how many tracks are local
-for item in tracks:
-    if item['is_local']:
-        tracks.remove(item)
-        i += 1
-# print result
-print("Playlist length: " + str(len(tracks)) + "\nExcluding: " + str(i))
-print(tracks[0]['track']['id'])
-print(tracks[0]['track']['name'])
-# TODO: изучить https://github.com/plamere/spotipy/blob/master/examples/audio_analysis_for_track.py
-#       посмотреть на тему поиск по BPM
+# print("Getting playlist...")
+# tracks = []
+# result = sp.playlist("37i9dQZF1DXdbXrPNafg9d", fields=['id', 'name', 'uri'])
+# print(f"Playlist {result}")
+# result = sp.playlist_items("37i9dQZF1DXdbXrPNafg9d", additional_types=['track'])
+# tracks.extend(result['items'])
+# # if playlist is larger than 100 songs, continue loading it until end
+# while result['next']:
+#     print("Continuing...")
+#     result = sp.next(result)
+#     tracks.extend(result['items'])
+# # remove all local songs
+# i = 0  # just for counting how many tracks are local
+# for item in tracks:
+#     if item['is_local']:
+#         tracks.remove(item)
+#         i += 1
+# # print result
+# print("Playlist length: " + str(len(tracks)) + "\nExcluding: " + str(i))
+# print(tracks[0]['track']['id'])
+# print(tracks[0]['track']['name'])
+# #analysis = sp.audio_analysis(tracks[0]['track']['id'])
+# #pprint(analysis['sections'])
+# #features = sp.audio_features([tracks[0]['track']['id']])
+# #pprint(features)
+
+api = get_music_provider("spotify")
+api.login()
+#print(api.calc_bpm_histogram(tracks[0]))
+#pprint(api.get_or_create_playlist("37i9dQZF1DXdbXrPNafg9d"))
+#pprint(api.get_or_create_my_playlist("bpmcrawl.180- - Google Play Музыка"))
+#pprint(api.get_playlist_tracks(api.get_or_create_my_playlist("test")))
+#pprint(api.add_track_to_playlist(api.get_or_create_my_playlist("test"), "2QvA4PTJzMYRBQeOVXLxhi"))
+pprint(api.get_or_create_my_playlist("test2"))
